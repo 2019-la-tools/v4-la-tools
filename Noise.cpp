cea49b8d628fa9854807992df93dff81ab7a2fa5
Brennan Kuhman <bkuhman@asu.edu>
2019-03-14 14:30:42 -0700

    Generate Gaussian noise with Box-Mueller Method

--- Noise.cpp
+++ Noise.cpp
@@ -17,6 +17,23 @@ float Noise::addNoise(float mean, float range) {
 		return noisy;
 	}else if (this->type == GAUSSIAN)
 	{
-		
+		const double PI = 3.14159265358979323846; //not declared in cmath.
+		float u, v;
+		do{
+			u = static_cast <float> (rand()) / static_cast <float> (RAND_MAX); //generate U on [0, 1).
+		}while(u == 0); // force it to (0, 1) while maintaining distribution.
+
+		do{
+			v = static_cast <float> (rand()) / static_cast <float> (RAND_MAX); //generate V on [0, 1).
+		}while(v == 0); // force it to (0, 1) while maintaining distribution.
+
+		//Use the Box-Mueller Method to generate a gaussian, or normally distributed number.
+		//We only generate X because we don't need to generate a second number.
+		float x = sqrt((-2) * log(u)) * (cos(2 * PI * v)); // x is now a normally distributed random number.
+		float noisy = mean + x; // apply noise to value.
+
+		cout << "value:noisy: " << mean << "\t" << x << endl;
+
+		return noisy;
 	}
 }
